4674625ae895951384b50941c453ed64cc8eb0a4
jiyeon0715 <jykang560@gmail.com>
2024-06-08 23:14:38 +0900

    fix : code : app

--- gui/client/app.py
+++ gui/client/app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import requests
 import time
+import base64
 import cv2
-import pandas as pd
+import os
 
 from http import HTTPStatus
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 from ultralytics import YOLO
-from datetime import datetime
+from utils.yoloProcess import YOLOProcessor
+from datetime import datetime, timedelta
 
 class Page:
     def __init__(self):
@@ -16,7 +19,7 @@ class Page:
     def set_page(self, page_name):
         st.session_state.page = page_name
         st.experimental_rerun()
-
+        
     def sign_in_page(self):
         st.set_page_config(page_title='Park Management', page_icon='images/favicon.ico', layout='centered')
 
@@ -103,27 +106,29 @@ class Page:
     def admin_page(self):
         st.set_page_config(page_title='Park Management', page_icon='images/favicon.ico', layout='wide', initial_sidebar_state='expanded')
         st.title('Park Management System')
-        self.pages = { 'LOG': self.log_page, 'CCTV': self.cctv_page }
-        page = st.sidebar.selectbox('선택', list(self.pages.keys()))
+        self.pages = {  'LOG': self.log_page, 'CCTV': self.cctv_page, }
+        page = st.sidebar.selectbox('Select Page', list(self.pages.keys()))
         self.pages[page]()
     
+
     def cctv_page(self):
         st.subheader("CCTV")
-        sidebar_cols = st.sidebar.columns(2)
-        with sidebar_cols[0]:
-            st.date_input('시작일', disabled=True)
-        with sidebar_cols[1]:
-            st.date_input('종료일', disabled=True)
-        st.sidebar.multiselect('필터', options=['A'], placeholder="선택해주세요.", disabled=True)
-        st.sidebar.button('적용', use_container_width=True, disabled=True)
         tabs = st.tabs(['A구역','CCTV 구역 추가'])
         with tabs[0]:
             st.header('A구역')
-            cctv1_path = "videos/action1.mp4"
+            cctv1_path = "/home/kjy/Downloads/test.mp4"
             self.cctv1(cctv1_path)
         with tabs[1]:
             st.header('CCTV 추가')
             self.upload_video()
+
+    # def model_paths(self):
+    #     model_paths = {
+    #         '모델 1': '/home/kjy/Downloads/streamlit-community-main/client/models/best.pt',
+    #         '모델 2': '/home/kjy/Downloads/streamlit-community-main/client/models/best (1).pt',
+    #         '모델 3': '/home/kjy/Downloads/streamlit-community-main/client/models/yolov8n-pose.pt'
+    #     }
+    #     return model_paths
 
     @st.cache_resource
     def load_model(_self, _model_path):
@@ -134,27 +139,133 @@ class Page:
         banner_detected = False
         banner_start_time = None
         banner_end_time = None
-        model = self.load_model('models/best.pt')
+
+        trash_detected = False
+        trash_start_time = None
+        trash_end_time = None
+
+        model1 = self.load_model('models/best.pt')
+        model2 = self.load_model('models/best (1).pt')
+        model3 = self.load_model('models/yolov8n-pose.pt')
+
         cap = cv2.VideoCapture(video_path)
         frame_window = st.image([])
+        
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = None
+        saving = False
+        frame_count = 0
+
+        save_dir = "/home/kjy/Downloads/saved_videos"
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+            
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
-            results = model(frame)[0]
+            
+            fixed_size = (640, 480)
+            frame = cv2.resize(frame, fixed_size)
+            frame_height, frame_width = frame.shape[:2]
+
+            results = model1(frame)[0]
             for result in results.boxes.data:
                 x1, y1, x2, y2, conf, cls = result
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                label = f"{model.names[int(cls)]} {conf:.2f}"
+                label = f"{model1.names[int(cls)]} {conf:.2f}"
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                if model.names[int(cls)] == 'banner_per':
+
+            results = model2(frame)[0]
+            for result in results.boxes.data:
+                x1, y1, x2, y2, conf, cls = result
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                label = f"{model2.names[int(cls)]} {conf:.2f}"
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+            results = model3(frame)[0]
+            for result in results.boxes.data:
+                x1, y1, x2, y2, conf, cls = result
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                label = f"{model3.names[int(cls)]} {conf:.2f}"
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+            results = model3(frame)[0]
+            for result in results.boxes.data:
+                x1, y1, x2, y2, conf, cls = result
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                label = f"{model3.names[int(cls)]} {conf:.2f}"
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+                if model1.names[int(cls)] == 'banner_per':
                     if not banner_detected:
                         banner_detected = True
                         banner_start_time = datetime.now()
+
+                        video_file = os.path.join(save_dir, f"detected_banner_{banner_start_time.strftime('%Y%m%d_%H%M%S')}.avi")
+                        out = cv2.VideoWriter(video_file, fourcc, 20.0, (frame_width, frame_height))
+                        if not out.isOpened():
+                            st.error("Error: Failed to open video writer")
+                        else:
+                            st.info(f"Video recording started: {video_file}")
+                        saving = True
+                        frame_count = 0
+
+                if model2.names[int(cls)] == 'dropped_trash':
+                    if not trash_detected:
+                        trash_detected = True
+                        trash_start_time = datetime.now()
+
+                        video_file = os.path.join(save_dir, f"detected_trash_{trash_start_time.strftime('%Y%m%d_%H%M%S')}.avi")
+                        out = cv2.VideoWriter(video_file, fourcc, 20.0, (frame_width, frame_height))
+                        if not out.isOpened():
+                            st.error("Error: Failed to open video writer")
+                        else:
+                            st.info(f"Video recording started: {video_file}")
+                        saving = True
+                        frame_count = 0
+
+            if banner_detected:
+                banner_end_time = datetime.now()
+                if saving:
+                    out.write(frame)
+                    frame_count += 1
+                if banner_end_time - banner_start_time > timedelta(seconds=30):
+                    saving = False
+                    if out:
+                        out.release()
+                        st.info(f"Video recording stopped: {video_file}")
+                        st.info(f"Total frames written: {frame_count}")
+                    st.info(f"Banner detected from {banner_start_time.strftime('%H:%M:%S')} to {banner_end_time.strftime('%H:%M:%S')}")
+                    banner_detected = False
+
+            if trash_detected:
+                trash_end_time = datetime.now()
+                if saving:
+                    out.write(frame)
+                    frame_count += 1
+                if trash_end_time - trash_start_time > timedelta(seconds=30):
+                    saving = False
+                    if out:
+                        out.release()
+                        st.info(f"Video recording stopped: {video_file}")
+                        st.info(f"Total frames written: {frame_count}")
+                    st.info(f"trash detected from {trash_start_time.strftime('%H:%M:%S')} to {trash_end_time.strftime('%H:%M:%S')}")
+                    trash_detected = False
+
             frame_window.image(frame, channels='BGR')
+
         cap.release()
+        if out is not None:
+            out.release()  
         
     def upload_video(self):
         st.subheader('CCTV ADD')
@@ -170,32 +281,10 @@ class Page:
     def log_page(self):
         sidebar_cols = st.sidebar.columns(2)
         with sidebar_cols[0]:
-            start_date = st.date_input('시작일')
+            st.date_input('시작일',)
         with sidebar_cols[1]:
-            end_date = st.date_input('종료일')
-        self.option = st.sidebar.multiselect('필터', ['A', 'B', '흡연', '쓰레기무단투기', '현수막', '음주'], max_selections=3, placeholder="선택해주세요.")
-        st.sidebar.button('적용', use_container_width=True, on_click=lambda: self.apply(start_date, end_date))
-
-        layouts = st.columns([4, 6])
-        with layouts[0]:
-            st.write('내역')
-            self.log_layout = st.container(height=600, border=True)
-        with layouts[1]:
-            st.write('영상')
-            self.video_layout = st.container(height=600, border=True)
-
-    def apply(self, start_date, end_date):
-        params = {'start_date': str(start_date), 'end_date': str(end_date)}
-        response = requests.post(st.secrets.address.address + '/log/download', json=params)
-        log = response.json()
-
-        df = pd.DataFrame(log, columns=['날짜', '구역', '시작시간', '종료시간', '행위'])
-        df = df.set_index('날짜')
-        with self.log_layout:
-            st.dataframe(df, use_container_width=True, on_select=self.clip)
-
-    def clip(self):
-        pass
+            st.date_input('종료일')
+        st.sidebar.button('적용', use_container_width=True)
 
     def run(self):
         if st.session_state.page == 'sign_in':
