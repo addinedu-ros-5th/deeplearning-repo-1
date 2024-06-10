e5c888febe35636589f6eb7ed2df7c3293b71dad
goosebomb <as379907as@gmail.com>
2024-06-10 11:05:01 +0900

    fix : code : model

--- gui/client/utils/model.py
+++ gui/client/utils/model.py
@@ -2,6 +2,6 @@ import streamlit as st
 from ultralytics import YOLO
 
 @st.cache_resource
-def load_model(model_path):
+def load_yolo_model(model_path):
     model = YOLO(model_path)
     return model
