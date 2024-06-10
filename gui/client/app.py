2d6a33989e26fdef8642bca6426e4739d42ced99
goosebomb <as379907as@gmail.com>
2024-06-10 11:35:07 +0900

    fix : code : app

--- gui/client/app.py
+++ gui/client/app.py
@@ -1,5 +1,3 @@
-import os
-import cv2
 import time
 import requests
 import pandas as pd
@@ -7,8 +5,7 @@ import streamlit as st
 
 from http import HTTPStatus
 from utils.file import find_file
-from utils.model import load_yolo_model
-from datetime import datetime, timedelta
+from utils.video import process
 
 class Page:
     def __init__(self):
@@ -107,37 +104,42 @@ class Page:
         st.title('Park Management System')
         self.pages = { 'RECORD': self.record_page, 'CCTV': self.cctv_page }
         page = st.sidebar.selectbox('Select Page', list(self.pages.keys()))
+        
+        if page == 'RECORD':
+            sidebar_cols = st.sidebar.columns(2)
+            with sidebar_cols[0]:
+                start_date = st.date_input('시작일')
+            with sidebar_cols[1]:
+                end_date = st.date_input('종료일')
+            section = st.sidebar.multiselect('구역', ['A'], default=None, placeholder='선택해주세요.')
+            action = st.sidebar.multiselect('행위', ['흡연', '투기', '음주'], default=None, placeholder='선택해주세요.')
+            st.sidebar.button('적용', use_container_width=True, on_click=lambda: self.apply(start_date, end_date, section, action))
+        elif page == 'CCTV':
+            sidebar_cols = st.sidebar.columns(2)
+            with sidebar_cols[0]:
+                st.date_input('시작일', disabled=True)
+            with sidebar_cols[1]:
+                st.date_input('종료일', disabled=True)
+            st.sidebar.multiselect('구역', ['A'], default=None, placeholder='선택해주세요.', disabled=True)
+            st.sidebar.multiselect('행위', ['A', '흡연', '투기', '음주'], default=None, placeholder='선택해주세요.', disabled=True)
+            st.sidebar.button('적용', use_container_width=True, on_click=lambda: self.apply(start_date, end_date, section, action), disabled=True)
+
         self.pages[page]()
 
     def cctv_page(self):
         st.subheader("CCTV")
-        sidebar_cols = st.sidebar.columns(2)
-        with sidebar_cols[0]:
-            st.date_input('시작일', disabled=True)
-        with sidebar_cols[1]:
-            st.date_input('종료일', disabled=True)
-        st.sidebar.multiselect('필터', ['A', '흡연', '투기', '음주'], default=None, placeholder='선택해주세요.', disabled=True)
-        st.sidebar.button('적용', use_container_width=True, disabled=True)
-
+        
         tabs = st.tabs(['A구역','CCTV 구역 추가'])
         with tabs[0]:
             st.header('A구역')
             cctv1_path = "videos/2024-01-01_123020.mp4"
-            self.cctv1(cctv1_path)
+            process(cctv1_path)
         with tabs[1]:
             st.header('CCTV 추가')
             self.upload_video()
 
     def record_page(self):
         st.subheader('RECORD')
-        sidebar_cols = st.sidebar.columns(2)
-        with sidebar_cols[0]:
-            start_date = st.date_input('시작일')
-        with sidebar_cols[1]:
-            end_date = st.date_input('종료일')
-        section = st.sidebar.multiselect('구역', ['A'], default=None, placeholder='선택해주세요.')
-        action = st.sidebar.multiselect('행위', ['흡연', '투기', '음주'], default=None, placeholder='선택해주세요.')
-        st.sidebar.button('적용', use_container_width=True, on_click=lambda: self.apply(start_date, end_date, section, action))
 
         layouts = st.columns([4, 6])
 
@@ -175,139 +177,7 @@ class Page:
         log_df = log_df.set_index(keys='날짜')
 
         st.session_state.df = log_df
-    
-    def cctv1(self, video_path):
-        banner_detected = False
-        banner_start_time = None
-        banner_end_time = None
 
-        trash_detected = False
-        trash_start_time = None
-        trash_end_time = None
-
-        model1 = load_yolo_model('models/best.pt')
-        model2 = load_yolo_model('models/best (1).pt')
-        model3 = load_yolo_model('models/yolov8n-pose.pt')
-
-        cap = cv2.VideoCapture(video_path)
-        frame_window = st.image([])
-        
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = None
-        saving = False
-        frame_count = 0
-
-        save_dir = "videos/"
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-            
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            
-            fixed_size = (1280, 720)
-            frame = cv2.resize(frame, fixed_size)
-            frame_height, frame_width = frame.shape[:2]
-
-            results = model1(frame)[0]
-            for result in results.boxes.data:
-                x1, y1, x2, y2, conf, cls = result
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                label = f"{model1.names[int(cls)]} {conf:.2f}"
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-            results = model2(frame)[0]
-            for result in results.boxes.data:
-                x1, y1, x2, y2, conf, cls = result
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                label = f"{model2.names[int(cls)]} {conf:.2f}"
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-            results = model3(frame)[0]
-            for result in results.boxes.data:
-                x1, y1, x2, y2, conf, cls = result
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                label = f"{model3.names[int(cls)]} {conf:.2f}"
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-            results = model3(frame)[0]
-            for result in results.boxes.data:
-                x1, y1, x2, y2, conf, cls = result
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                label = f"{model3.names[int(cls)]} {conf:.2f}"
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-                if model1.names[int(cls)] == 'banner_per':
-                    if not banner_detected:
-                        banner_detected = True
-                        banner_start_time = datetime.now()
-
-                        video_file = os.path.join(save_dir, f"detected_banner_{banner_start_time.strftime('%Y-%m-%d_%H%M%S')}.avi")
-                        out = cv2.VideoWriter(video_file, fourcc, 20.0, (frame_width, frame_height))
-                        if not out.isOpened():
-                            st.error("Error: Failed to open video writer")
-                        else:
-                            st.info(f"Video recording started: {video_file}")
-                        saving = True
-                        frame_count = 0
-
-                if model2.names[int(cls)] == 'dropped_trash':
-                    if not trash_detected:
-                        trash_detected = True
-                        trash_start_time = datetime.now()
-
-                        video_file = os.path.join(save_dir, f"detected_trash_{trash_start_time.strftime('%Y-%m-%d_%H%M%S')}.avi")
-                        out = cv2.VideoWriter(video_file, fourcc, 20.0, (frame_width, frame_height))
-                        if not out.isOpened():
-                            st.error("Error: Failed to open video writer")
-                        else:
-                            st.info(f"Video recording started: {video_file}")
-                        saving = True
-                        frame_count = 0
-
-            if banner_detected:
-                banner_end_time = datetime.now()
-                if saving:
-                    out.write(frame)
-                    frame_count += 1
-                if banner_end_time - banner_start_time > timedelta(seconds=30):
-                    saving = False
-                    if out:
-                        out.release()
-                        st.info(f"Video recording stopped: {video_file}")
-                        st.info(f"Total frames written: {frame_count}")
-                    st.info(f"Banner detected from {banner_start_time.strftime('%H:%M:%S')} to {banner_end_time.strftime('%H:%M:%S')}")
-                    banner_detected = False
-
-            if trash_detected:
-                trash_end_time = datetime.now()
-                if saving:
-                    out.write(frame)
-                    frame_count += 1
-                if trash_end_time - trash_start_time > timedelta(seconds=30):
-                    saving = False
-                    if out:
-                        out.release()
-                        st.info(f"Video recording stopped: {video_file}")
-                        st.info(f"Total frames written: {frame_count}")
-                    st.info(f"trash detected from {trash_start_time.strftime('%H:%M:%S')} to {trash_end_time.strftime('%H:%M:%S')}")
-                    trash_detected = False
-
-            frame_window.image(frame, channels='BGR')
-
-        cap.release()
-        if out is not None:
-            out.release()  
-        
     def upload_video(self):
         st.subheader('CCTV ADD')
         uploaded_file = st.file_uploader('Select CCTV to add', type=['mp4'])
