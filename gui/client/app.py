682a2d34f1a15feabb18cbaa685e5498e3d2cbbf
goosebomb <as379907as@gmail.com>
2024-06-11 20:54:48 +0900

    fix : code : app

--- gui/client/app.py
+++ gui/client/app.py
@@ -173,8 +173,8 @@ class Page:
 
         log_data = response.json()
 
-        log_df = pd.DataFrame(log_data, columns=['날짜 / 시간', '구역', '행위'])
-        log_df = log_df.set_index(keys='날짜 / 시간')
+        log_df = pd.DataFrame(log_data, columns=['날짜', '시간', '구역', '행위'])
+        log_df = log_df.set_index(keys='날짜')
 
         st.session_state.df = log_df
 
