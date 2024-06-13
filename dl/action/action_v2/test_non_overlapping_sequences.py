697195d1fc01a66a5bde1447c536a91c3554630f
1yohanyo1 <yohangim119@gmail.com>
2024-06-14 03:34:02 +0900

    create : code : test_non_overlapping_sequences.py

---
+++ dl/action/action_v2/test_non_overlapping_sequences.py
@@ -0,0 +1,74 @@
+from tensorflow.keras.models import load_model
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# 저장된 LSTM 모델 파일 경로
+model_path = 'lstm_model.keras'
+
+# 모델 불러오기
+lstm_model = load_model(model_path)
+
+
+# CSV 파일 읽기
+df = pd.read_csv('deeplearning_demo.csv')
+
+# 시퀀스 길이 설정 (예: 36)
+sequence_length = 10
+
+# 시퀀스를 생성하는 함수
+def create_non_overlapping_sequences(data, seq_length):
+    sequences = []
+    labels = []
+    i = 0
+    while i + seq_length <= len(data):
+        seq = data.iloc[i:i + seq_length, :-1].values  # 시퀀스 데이터
+        label = data.iloc[i + seq_length - 1, -1]      # 시퀀스의 마지막 라벨
+        sequences.append(seq)
+        labels.append(label)
+        i += seq_length
+    return np.array(sequences), np.array(labels)
+
+
+
+# 시퀀스 생성
+X, y = create_non_overlapping_sequences(df, sequence_length)
+
+# 시퀀스 길이에 맞추어 데이터 정제 (예: 패딩 추가)
+
+X = pad_sequences(X, maxlen=sequence_length, dtype='float32', padding='post')
+
+
+# 레이블 인코딩 (1 -> 0, 4 -> 1)
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+# 고유한 클래스 수 확인
+num_classes = len(np.unique(y_encoded))
+
+# 원-핫 인코딩
+y_one_hot = to_categorical(y_encoded, num_classes=num_classes)
+
+# LSTM 모델 입력 형식 맞추기
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.2, random_state=42)
+X_train = X_train.reshape((-1, sequence_length, 34))  # 34는 스켈레톤 좌표의 특성 수입니다.
+X_test = X_test.reshape((-1, sequence_length, 34))
+
+# 모델 정의와 학습 코드에 이 입력 형식을 맞추어 학습합니다.
+
+# 모델 평가
+loss, accuracy = lstm_model.evaluate(X_test, y_test)
+print(f'Test Loss: {loss}')
+print(f'Test Accuracy: {accuracy}')
+
+# 예측 수행
+y_pred = lstm_model.predict(X_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+# 예측 결과 출력
+print(f'Predicted classes: {y_pred_classes}')
+print(f'True classes: {np.argmax(y_test, axis=1)}')
+
